5cadd4135291100230f66a703f8af23a5aed1008
MagikBased <124455610+MagikBased@users.noreply.github.com>
2025-02-18 23:32:39 -0600

    Added smoothing to movement and acceleration, and coyote time and jump buffering.

--- BrackeyJam2025/Assets/_scripts/PlayerMovement.cs
+++ BrackeyJam2025/Assets/_scripts/PlayerMovement.cs
@@ -1,57 +1,123 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
     [Header("Movement")]
     public bool canMove = true;
-    public float moveSpeed = 5f;
-    private float xInput = 0f;
+    public float moveSpeed = 8f;
+    public float acceleration = 10f;
+    public float deceleration = 10f;
+    public float airControlMultiplier = 0.5f;
+    private float moveInput;
+    private float targetSpeed;
+    private float speedDiff;
+    private float accelRate;
 
-    [Header("Jump")]
-    public bool  canJump = true;
-    public float jumpForce = 100f;
-    public bool  isGrounded { get; private set; }
+    [Header("Jumping")]
+    public bool canJump = true;
+    public float jumpForce = 12f;
+    public float jumpTimeMax = 0.3f;
+    private float jumpTime;
+    private bool isJumping;
+
+    [Header("Jump Assistance")]
+    public float coyoteTime = 0.2f;
+    public float jumpBufferTime = 0.2f;
+    private float coyoteTimeCounter;
+    private float jumpBufferCounter;
+
+
+    [Header("Ground Check")]
     public Transform groundCheck;
+    public bool isGrounded { get; private set; }
     public float groundCheckRadius = 0.2f;
     public LayerMask groundLayer;
-    
+
     private Rigidbody2D RB;
 
     private void Awake()
     {
         RB = GetComponent<Rigidbody2D>();
+        RB.interpolation = RigidbodyInterpolation2D.Interpolate;
     }
 
-    // Update is called once per frame
     private void Update()
     {
         CheckGrounded();
 
-        //Player 'A', 'D', right arrow, left arrow, and remote controller stick movement
         if (canMove)
-            xInput = Input.GetAxis("Horizontal");
-        
-        if (canJump && Input.GetButtonDown("Jump"))
-            PlayerJump();
+            moveInput = Input.GetAxisRaw("Horizontal");
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jumpBufferCounter = jumpBufferTime;
+        }
+        else
+        {
+            jumpBufferCounter -= Time.deltaTime;
+        }
+
+        if (isGrounded)
+        {
+            coyoteTimeCounter = coyoteTime;
+        }
+        else
+        {
+            coyoteTimeCounter -= Time.deltaTime;
+        }
+
+        if (canJump && jumpBufferCounter > 0 && coyoteTimeCounter > 0)
+        {
+            isJumping = true;
+            jumpTime = jumpTimeMax;
+            RB.velocity = new Vector2(RB.velocity.x, jumpForce);
+            jumpBufferCounter = 0;
+        }
+
+        if (Input.GetButton("Jump") && isJumping)
+        {
+            if (jumpTime > 0)
+            {
+                RB.velocity = new Vector2(RB.velocity.x, jumpForce);
+                jumpTime -= Time.deltaTime;
+            }
+            else
+            {
+                isJumping = false;
+            }
+        }
+
+        if (Input.GetButtonUp("Jump"))
+        {
+            isJumping = false;
+            if (RB.velocity.y > 0)
+            {
+                RB.velocity = new Vector2(RB.velocity.x, RB.velocity.y * 0.5f);
+            }
+        }
     }
 
     private void FixedUpdate()
     {
-        RB.velocity = new Vector2(xInput * moveSpeed, RB.velocity.y);
+        ApplyMovement();
     }
 
-    private void PlayerJump()
+    private void ApplyMovement()
     {
-        if (isGrounded)
-        {
-            RB.AddForce(new Vector2(0, jumpForce));
-        }
+        targetSpeed = moveInput * moveSpeed;
+        speedDiff = targetSpeed - RB.velocity.x;
+        accelRate = (isGrounded ? acceleration : acceleration * airControlMultiplier);
+        float movement = Mathf.Lerp(RB.velocity.x, targetSpeed, accelRate * Time.fixedDeltaTime);
+        RB.velocity = new Vector2(movement, RB.velocity.y);
     }
 
     private void CheckGrounded()
     {
         isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+
+        if (isGrounded)
+        {
+            coyoteTimeCounter = coyoteTime;
+        }
     }
 }
