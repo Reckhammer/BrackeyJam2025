efdb62995ddee65779a389e305c8291e26ab9af3
Reckhammer <joshua.rechkemmer48@gmail.com>
2025-02-18 23:19:43 -0600

    Player can now only jump while on the ground, Made a prefab for the player

--- BrackeyJam2025/Assets/_scripts/PlayerMovement.cs
+++ BrackeyJam2025/Assets/_scripts/PlayerMovement.cs
@@ -4,28 +4,54 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float speed = 5;
-    public float jumpForce = 100;
-    private Rigidbody2D RB;
+    [Header("Movement")]
+    public bool canMove = true;
+    public float moveSpeed = 5f;
+    private float xInput = 0f;
 
+    [Header("Jump")]
+    public bool  canJump = true;
+    public float jumpForce = 100f;
+    public bool  isGrounded { get; private set; }
+    public Transform groundCheck;
+    public float groundCheckRadius = 0.2f;
+    public LayerMask groundLayer;
+    
+    private Rigidbody2D RB;
 
     private void Awake()
     {
         RB = GetComponent<Rigidbody2D>();
     }
-    // Update is called once per frame
-    void Update()
-    {
-        //Player 'A', 'D', right arrow, left arrow, and remote controller stick movement
-        float x = Input.GetAxis("Horizontal");
-        RB.velocity = new Vector2(x*speed, RB.velocity.y);
 
-        if (Input.GetButtonDown("Jump"))
+    // Update is called once per frame
+    private void Update()
+    {
+        CheckGrounded();
+
+        //Player 'A', 'D', right arrow, left arrow, and remote controller stick movement
+        if (canMove)
+            xInput = Input.GetAxis("Horizontal");
+        
+        if (canJump && Input.GetButtonDown("Jump"))
+            PlayerJump();
+    }
+
+    private void FixedUpdate()
+    {
+        RB.velocity = new Vector2(xInput * moveSpeed, RB.velocity.y);
+    }
+
+    private void PlayerJump()
+    {
+        if (isGrounded)
         {
-            RB.AddForce(new Vector2(0,jumpForce));
-            print("jump");
+            RB.AddForce(new Vector2(0, jumpForce));
         }
     }
 
-
+    private void CheckGrounded()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+    }
 }
