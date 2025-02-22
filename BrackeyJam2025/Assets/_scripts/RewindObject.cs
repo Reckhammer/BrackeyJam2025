a0bc5e2c1051b9c19b3beb865a29465b1866c621
MagikBased <124455610+MagikBased@users.noreply.github.com>
2025-02-22 11:26:19 -0600

    Worked on the rewind mechanic

--- BrackeyJam2025/Assets/_scripts/RewindObject.cs
+++ BrackeyJam2025/Assets/_scripts/RewindObject.cs
@@ -5,34 +5,102 @@ public class RewindObject : MonoBehaviour
 {
     public bool isRewinding = false;
     public float maxTimeRecordInSeconds = 5;
-    List<TimeTick> timeTicks;
-    Rigidbody2D rb;
+    private List<TimeTick> timeTicks;
+    private Rigidbody2D rb;
+    private Animator animator;
 
-    private void Start() 
+    void Start()
     {
         timeTicks = new List<TimeTick>();
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
-    private void FixedUpdate() 
+    void Update()
     {
-        timeTicks.Insert(0, new TimeTick(transform.position,transform. rotation, rb.velocity, rb.angularVelocity));
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            StartRewind();
+        }
 
-        if (timeTicks.Count > maxTimeRecordInSeconds * 100)
+        if (Input.GetKeyUp(KeyCode.R))
+        {
+            StopRewind();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (isRewinding)
+        {
+            Rewind();
+        }
+        else
+        {
+            RecordTimeTick();
+        }
+    }
+
+    private void RecordTimeTick()
+    {
+        timeTicks.Insert(0, new TimeTick(transform.position, transform.rotation, rb.velocity, rb.angularVelocity, GetCurrentAnimationHash()));
+
+        if (timeTicks.Count > maxTimeRecordInSeconds * 100 && maxTimeRecordInSeconds > 0)
         {
             timeTicks.RemoveAt(timeTicks.Count - 1);
         }
     }
-    public void Rewind()
+
+    public void StartRewind()
+    {
+        isRewinding = true;
+        rb.isKinematic = true;
+        rb.velocity = Vector2.zero;
+        rb.angularVelocity = 0f;
+        animator.enabled = false;
+    }
+
+    public void StopRewind()
+    {
+        isRewinding = false;
+        rb.isKinematic = false;
+        animator.enabled = true;
+        RestoreAnimationState();
+    }
+
+    private void Rewind()
+    {
+        if (timeTicks.Count > 1)
+        {
+            TimeTick currentTick = timeTicks[0];
+            TimeTick nextTick = timeTicks[1];
+
+            rb.MovePosition(Vector3.Lerp(transform.position, nextTick.position, Time.fixedDeltaTime * 15));
+            rb.MoveRotation(Quaternion.Lerp(transform.rotation, nextTick.quaternion, Time.fixedDeltaTime * 15));
+
+            rb.velocity = nextTick.velocity;
+            rb.angularVelocity = nextTick.angularVelocity;
+
+            animator.Play(currentTick.animationStateHash, 0, 0);
+
+            timeTicks.RemoveAt(0);
+        }
+        else
+        {
+            StopRewind();
+        }
+    }
+
+    private int GetCurrentAnimationHash()
+    {
+        return animator.GetCurrentAnimatorStateInfo(0).shortNameHash;
+    }
+
+    private void RestoreAnimationState()
     {
         if (timeTicks.Count > 0)
         {
-            TimeTick tick = timeTicks[0];
-            transform.position = tick.position;
-            transform.rotation = tick.quaternion;
-            rb.velocity = tick.velocity;
-            rb.angularVelocity = tick.angularVelocity;
-            timeTicks.RemoveAt(0);
+            animator.Play(timeTicks[0].animationStateHash, 0, 0);
         }
     }
 }
--- BrackeyJam2025/Assets/_scripts/TimeTick.cs
+++ BrackeyJam2025/Assets/_scripts/TimeTick.cs
@@ -1,17 +1,19 @@
 using UnityEngine;
 
-public class TimeTick : MonoBehaviour
+public class TimeTick
 {
     public Vector3 position;
     public Quaternion quaternion;
     public Vector3 velocity;
     public float angularVelocity;
+    public int animationStateHash;
 
-    public TimeTick (Vector3 pos, Quaternion rot, Vector3 vel, float angVel)
+    public TimeTick(Vector3 pos, Quaternion rot, Vector3 vel, float angVel, int animState)
     {
         position = pos;
         quaternion = rot;
         velocity = vel;
         angularVelocity = angVel;
+        animationStateHash = animState;
     }
 }
